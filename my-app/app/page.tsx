b07c6eb7cc3d6d14a902cca1b3c6bb01a9534690
douglasalex2002 <douglaad@dukes.jmu.edu>
2025-03-07 10:49:44 -0500

    Structure and some progress

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,101 +1,65 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className='colored-rectangle'></div>
+      <div id='top-row'>
+        <div className='top-row-left'>
+          <a href="#top-row" className="left-buttons" id='AD-button'>AD</a>
+          <a href="#AboutMe" className="left-buttons" id='hover-underline'>About Me</a>
+          <a href="#Experience" className="left-buttons" id='hover-underline'>Experience</a>
+          <a href="#Projects" className="left-buttons" id='hover-underline'>Projects</a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <div>
+          <a href="#ContactMe" className='topButtons' id="contact-me">Contact Me</a>
+        </div>
+      </div>
+      <div id='AboutMe'>
+        <img src="/pictures/headshot_circle.png" alt="Professional Headshot" width="450" height="450" className="about-me-items"/>
+        <p className="about-me-items" id="about-me-text">Hello, I am <span id="my-name">Alex Douglas</span>...</p>
+      </div>
+      <div id="featuredWork">
+        <h1 className="section-title">Featured Work</h1>
+        <div id="featured-work-projects">
+          <div className='single-featured-work'>
+            <img src='/pictures/placeholder.png' alt="Place Holder"></img>
+            <p className="featured-text">One</p>
+          </div>
+          <div className='single-featured-work'>
+            <img src='/pictures/placeholder.png' alt="Place Holder"></img>
+            <p className="featured-text">Two</p>
+          </div>
+          <div className='single-featured-work'>
+            <img src='/pictures/placeholder.png' alt="Place Holder"></img>
+            <p className="featured-text">Three</p>
+          </div>
+        </div>
+      </div>
+      <div id='Experience'>
+        <h1 className="section-title">
+          Experience
+        </h1>
+        <p>
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Morbi dictumst nam nam, maximus eu commodo. In sodales elementum vivamus torquent augue ultricies commodo augue. Viverra tellus vehicula porta ornare tristique egestas sit id? Elit eleifend diam eu porttitor rhoncus felis elementum vivamus et. Viverra eleifend dolor urna, iaculis penatibus etiam. Ipsum curabitur eu suscipit bibendum auctor conubia nibh.
+
+Conubia mollis eleifend maecenas sodales primis quis. Vestibulum etiam et ipsum mattis commodo vehicula congue ligula. Ipsum auctor nostra elementum, varius litora sapien risus lacus torquent. Pretium integer senectus rutrum lacus congue penatibus pretium sollicitudin. Vitae efficitur posuere curae commodo sodales aliquam orci quis aenean. Nam ullamcorper parturient sit sollicitudin netus arcu; consequat nunc libero. Non fusce sapien; justo taciti finibus lacus vestibulum parturient. Ad efficitur rhoncus rutrum, scelerisque dis vestibulum fermentum fusce. Elit euismod nibh inceptos donec volutpat.
+
+Amet etiam enim sem sit adipiscing. Nullam enim imperdiet egestas euismod, varius dis ante hac! Himenaeos augue est magna justo fermentum hac augue. Morbi tempus potenti dui rhoncus rutrum tempor facilisis. Dolor netus ullamcorper porta convallis sodales malesuada proin amet. Malesuada eleifend quis maecenas quam at mattis feugiat. Interdum lobortis eu enim nibh nisi vestibulum eu sodales. Volutpat pretium lorem taciti neque elit maecenas feugiat tellus.        </p>
+      </div>
+      <div id='Projects'>
+        <h1 className="section-title">
+          Projects
+        </h1>
+        <p>
+        Lorem ipsum odor amet, consectetuer adipiscing elit. Morbi dictumst nam nam, maximus eu commodo. In sodales elementum vivamus torquent augue ultricies commodo augue. Viverra tellus vehicula porta ornare tristique egestas sit id? Elit eleifend diam eu porttitor rhoncus felis elementum vivamus et. Viverra eleifend dolor urna, iaculis penatibus etiam. Ipsum curabitur eu suscipit bibendum auctor conubia nibh.
+
+Conubia mollis eleifend maecenas sodales primis quis. Vestibulum etiam et ipsum mattis commodo vehicula congue ligula. Ipsum auctor nostra elementum, varius litora sapien risus lacus torquent. Pretium integer senectus rutrum lacus congue penatibus pretium sollicitudin. Vitae efficitur posuere curae commodo sodales aliquam orci quis aenean. Nam ullamcorper parturient sit sollicitudin netus arcu; consequat nunc libero. Non fusce sapien; justo taciti finibus lacus vestibulum parturient. Ad efficitur rhoncus rutrum, scelerisque dis vestibulum fermentum fusce. Elit euismod nibh inceptos donec volutpat.
+
+Amet etiam enim sem sit adipiscing. Nullam enim imperdiet egestas euismod, varius dis ante hac! Himenaeos augue est magna justo fermentum hac augue. Morbi tempus potenti dui rhoncus rutrum tempor facilisis. Dolor netus ullamcorper porta convallis sodales malesuada proin amet. Malesuada eleifend quis maecenas quam at mattis feugiat. Interdum lobortis eu enim nibh nisi vestibulum eu sodales. Volutpat pretium lorem taciti neque elit maecenas feugiat tellus.        </p>
+      </div>
+    </>
+  )
 }
+
+export default page
